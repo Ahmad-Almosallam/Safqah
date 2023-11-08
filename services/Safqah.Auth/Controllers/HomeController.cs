7066d7e5c222f9302b655f1a2dddfe3344d39668
Ahmad-Almosallam <vprezn@gmail.com>
2023-11-08 21:33:34 +0300

    adding the auth project and JWT settings

---
+++ services/Safqah.Auth/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Safqah.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
