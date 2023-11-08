93373f997d5fd0e72b55bef333d68d4d5372606f
Ahmad-Almosallam <vprezn@gmail.com>
2023-11-08 22:33:43 +0300

    adding all the microservices

---
+++ services/Safqah.Investor/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Safqah.Investor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ services/Safqah.Opportunity/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Safqah.Opportunity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
