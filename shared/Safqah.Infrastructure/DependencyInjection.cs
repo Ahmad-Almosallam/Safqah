3976221b41036e90ef36cbd6dbd9b6b38d0c4059
amosallam <amosallam@sdaia.gov.sa>
2023-11-08 19:11:18 +0300

    Add project files.

---
+++ shared/Safqah.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Safqah.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<SafqahDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("Default"));
+            });
+
+        }
+    }
+}
---
+++ shared/Safqah.Infrastructure/SafqahDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Safqah.Infrastructure
+{
+    public class SafqahDbContext : IdentityDbContext
+    {
+
+        public SafqahDbContext(DbContextOptions<SafqahDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
